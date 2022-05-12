7e0d9b123fae71c10a4707a3c7dd58549050d9f1
alvigabriel <claudio.alves20112011@hotmail.com>
2022-05-12 13:25:57 -0300

    JavaScript - Primeiras aulas

---
+++ JavaScript/Aula 2.js
@@ -0,0 +1,4 @@
+/* pra falar com o console usa o comando console.log
+e o que quer ver entre parenteses*/
+
+console.log('Hello world');
---
+++ JavaScript/Primeira aula.js
@@ -0,0 +1,24 @@
+//variável e constante
+
+//numa constante, sempre usar maiúsculas
+
+const PRECO
+var desconto
+var total
+
+total = PRECO - desconto
+
+// funções
+
+/* os parêntesis servem pra declarar atributos
+da função*/
+
+function soma (a, b ) { 
+    return a + b;
+
+}
+
+soma(3, 5);
+
+/* OBS: é preciso sempre "chamar a função", ou seja,
+executa-la*/
