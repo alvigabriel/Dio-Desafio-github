c8f2510ae6bf4809dc1744093ab6be74190dcb7e
alvigabriel <claudio.alves20112011@hotmail.com>
2022-05-27 08:48:22 -0300

    Exercício módulos, 27/05

---
+++ JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Claudio', 25));
+console.log(mostraCidade('Claudio', 'Salvador'));
+console.log(mostraHobby('Claudio', 'jogar COD'));
